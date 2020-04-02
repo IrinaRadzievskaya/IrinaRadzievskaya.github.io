6ff4cc6309d6392ebc7d67f769cf7950b483b98c
Yaroslav <kznkv.test@gmail.com>
2020-04-02 18:59:15 +0300

    style slider

--- app.js
+++ app.js
@@ -36,3 +36,28 @@ for(let i = 0; i < likeButtons.length; i++) {
 		likeButtons[i].classList.toggle('product__control_like-active')
 	})
 }
+
+$('.slider-block').slick({
+	dots:true,
+})
+
+// change quantity
+let productQuantity = document.querySelectorAll('.product__quantity')
+
+for (let i = 0; i < productQuantity.length; i++) {
+	let quantityInput = productQuantity[i].querySelector('.product__quantity input')
+	let incrementBtn = productQuantity[i].querySelector('.button_increment')
+	let decrementBtn = productQuantity[i].querySelector('.button_decrement')
+	incrementBtn.addEventListener("click",function() {
+		quantityInput.value = Number(quantityInput.value) + 1
+		incrementBtn.disabled = quantityInput.value >= 5
+		decrementBtn.disabled = quantityInput.value <= 1
+	}) 
+	decrementBtn.addEventListener("click",function() {
+		quantityInput.value = Number(quantityInput.value) - 1
+		incrementBtn.disabled = quantityInput.value >= 5
+		decrementBtn.disabled = quantityInput.value <= 1
+	})
+}
+
+$('.catalog__sort').selectize();
